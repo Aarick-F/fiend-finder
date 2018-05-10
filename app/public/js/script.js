f29f79eef359aff6e08c0f9d9a6bfd38210fdd50
Aarick Farist <zeflyndesign@gmail.com>
2018-05-10 16:59:08 +0000

    added fields to survey, added logic that scrapes survey for answers

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,4 +1,35 @@
 $(document).ready(() => {
 
+  $("#submit").on("click", () => {
+    let answersArray = [];
+    
+    let a1 = $("#question-one")["0"].selectedOptions["0"].value;
+    let a2 = $("#question-two")["0"].selectedOptions["0"].value;
+    let a3 = $("#question-three")["0"].selectedOptions["0"].value;
+    let a4 = $("#question-four")["0"].selectedOptions["0"].value;
+    let a5 = $("#question-five")["0"].selectedOptions["0"].value;
+    let a6 = $("#question-six")["0"].selectedOptions["0"].value;
+    let a7 = $("#question-seven")["0"].selectedOptions["0"].value;
+    let a8 = $("#question-eight")["0"].selectedOptions["0"].value;
+    let a9 = $("#question-nine")["0"].selectedOptions["0"].value;
+    let a10 = $("#question-ten")["0"].selectedOptions["0"].value;
+    
+    answersArray.push(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+    
+    let name = $("#name").val().trim();
+    let image = $("#image").val().trim();
+    
+    let newFiend = new Fiend(name, image, answersArray);
+    
+    console.log(newFiend);
+  });
+  
+  class Fiend {
+    constructor(name, image, answers) {
+      this.name = name;
+      this.image = image;
+      this.answers = answers;
+    }
+  }
 
 });
